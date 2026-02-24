f2b1a6873c6b41bf9f6e48a1b7da6163fb5742ab
SofiaRubioC-0317 <dianas.rubioc@autonoma.edu.co>
2026-02-23 19:46:30 -0500

    Documentacio Exceptions

--- Heroes-Parcial/src/main/java/org/example/Exceptiones/NoCompatibleException.java
+++ Heroes-Parcial/src/main/java/org/example/Exceptiones/NoCompatibleException.java
@@ -1,6 +1,11 @@
 package org.example.Exceptiones;
-
+/** * Excepción lanzada cuando un héroe intenta realizar una acción
+ * para la cual no tiene la habilidad o interfaz necesaria.
+ */
 public class NoCompatibleException extends Exception  {
+    /** * Excepción lanzada cuando un héroe intenta realizar una acción
+     * para la cual no tiene la habilidad o interfaz necesaria.
+     */
     public NoCompatibleException(String mensaje) {
         super(mensaje);
     }
